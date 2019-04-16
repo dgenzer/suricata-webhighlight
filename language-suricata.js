9643063e4ac8a94223a14060bc3535a09ea79e07
Dominik Genzer <genzer@posteo.de>
2019-04-16 16:28:21 +0200

    improvements for strings and sticky modifiers

--- language-suricata.js
+++ language-suricata.js
@@ -2,19 +2,22 @@ Prism.languages.suricata = {
   comment: /#.*/,
   'invalid bold': /(\$[A-Z0-9_]*[a-z]+[A-Z0-9_]*|<\-)/,
   constant: /(\$[A-Z0-9_]+)/,
-  "operator bold": /(pass|drop|reject|alert)/,
+  "operator bold": /\s*(pass|drop|reject|alert)\s+/,
   tag: /(any|tcp|udp|icmp|ip|http|ftp|tls|smb|dns|dcerpc|ssh|smtp|imap|msn|modbus|dnp3|enip|nfs|ikev2|krb5|ntp|dhcp)\b/,
   url: RegExp("url,([^;]+)", "i"),
   string: {
     pattern: /(:)\"[^\"]+\"/,
-    lookbehind: true
-
+    lookbehind: true,
+    greedy: true 
   },
   property: {
-    pattern: /(:)[^;]+/,
+    pattern: /[^;:]+;/,
     lookbehind: true,
   },
-  keyword: /([a-z\_]+):/,
+  keyword: {
+    pattern: /([a-z\_]+):/,
+    greedy: true
+  },
   symbol: /(<>|\->)/,
   punctuation: /[{}[\];(),.:]/,
   number: {
